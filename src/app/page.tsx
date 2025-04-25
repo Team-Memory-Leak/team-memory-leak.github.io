8d6338f54a993781f7f66f82c30f63f92e4b9584
lbojo006 <luisbojorquezgoal@gmail.com>
2025-04-25 01:27:07 -0700

    Converted our HTML page into a react page using Node.js

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="grid grid-rows-2 justify-center items-center gap-2">
+      <div>This is the New React Website for Team Memory Leak</div>
+      <div className="flex justify-center">Currently In Progress</div> 
+    </div>
+  )
+}
+
+export default page
