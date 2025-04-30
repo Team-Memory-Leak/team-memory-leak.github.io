e662d7e82377cccdd68e938edb6d54d9c3955bd8
lbojo006 <luisbojorquezgoal@gmail.com>
2025-04-30 16:22:12 -0700

    Added Default Pages For our website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const page = () => {
   return (
     <div className="grid grid-rows-2 justify-center items-center gap-2">
---
+++ src/app/teammembers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const teammembers = () => {
+  return (
+    <div>teammembers</div>
+  )
+}
+
+export default teammembers
