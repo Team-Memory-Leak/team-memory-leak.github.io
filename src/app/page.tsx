44b461219cb16702cbda7c6f55e580e998e63b45
lbojo006 <luisbojorquezgoal@gmail.com>
2025-05-05 19:08:27 -0700

    Fixed Husky Prepush

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ const page = () => {
     <>
       <div className="grid grid-rows-2 items-center justify-center gap-2">
         <div>This is the New React Website for Team Memory Leak</div>
+
         <div className="flex justify-center">Currently In Progress</div>
       </div>
     </>
